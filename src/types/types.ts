c1bb66b7ee57523e1fb89eaa3d390a3c15efe1a7
James <jamestariga08@live.com>
2022-10-17 21:51:57 -0700

    🔥 Added a new type and interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,18 @@
-export default interface DogsRequest {
+export type DogsRequest = {
   message: string[]
   status: string
+}
+
+type PopulationRequest = {
+  'ID Nation': string
+  'ID Year': number
+  Nation: string
+  Population: number
+  'Slug Nation': string
+  Year: string
+}
+
+export interface Population {
+  data: PopulationRequest[]
+  source: {}[]
 }
